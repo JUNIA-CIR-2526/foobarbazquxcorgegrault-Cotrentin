cb3ba7f60cdbfcdabb688ce56ba7f247b6e4a49d
Coco <corentindubernet1@gmail.com>
2025-09-21 18:55:26 +0200

    resolution exercice

---
+++ src/main/java/com/jad/Foo.java
@@ -0,0 +1,23 @@
+package com.jad;
+
+public class Foo {
+
+    private final Bar bar;
+    private Baz[] bazs;
+    private Qux qux;
+    private Corge corge;
+    private Grault[] graults;
+
+    public Foo(Bar bar) {
+        this.bar = bar;
+    }
+
+    public void addBaz(Baz baz) {
+
+    }
+
+    public void addGrault() {
+
+    }
+
+}
---
+++ src/main/java/com/jad/Grault.java
@@ -0,0 +1,11 @@
+package com.jad;
+
+public abstract class Grault {
+
+    private final Foo foo;
+
+    public Grault(Foo foo) {
+        this.foo = foo;
+    }
+
+}
